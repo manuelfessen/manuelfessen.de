8bf336161d0596a2b178efd7cfefb88c384c68d5
Fessen, Manuel <manuel.fessen@gmail.com>
2022-07-08 16:03:21 +0200

    finished navbar 2.0 finally
    hopefully... :smile:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,17 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: [
     "./public/**/*.html",
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-fluid-type")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-fluid-type"), require("daisyui")],
   corePlugins: {
     fontSize: false,
   },
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-      },
-      textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
-      },
-      backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
-      },
-      borderColor: {
-        default: "var(--color-border)",
-      },
-    },
+  daisyui: {
+    themes: [
+      "night",
+      "cyberpunk",
+    ],
+    darkTheme: "night", // this will be the default dark theme
   },
 };
