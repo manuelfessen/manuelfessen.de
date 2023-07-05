25cb2f9752312578f29c7295fafa64febbffd341
Manuel Fessen <manuel.fessen@easy.de>
2023-07-05 16:30:44 +0200

    update components - fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -52,5 +52,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
+  plugins: [require('@tailwindcss/typography')],
 }
