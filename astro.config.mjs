caeacc3cb8e482a29ccf7b040987459bab6aae39
Manuel Fessen <manuel.fessen@gmail.com>
2025-02-05 22:35:35 +0100

    no about

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,36 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
-import config from './vendor/integration';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const hasExternalScripts = false;
 const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://manuelfessen.de',
+  base: '/',
+  trailingSlash: 'always',
   output: 'static',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), config({
-    config: "./src/config.yaml"
-  })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    sitemap(), 
+    mdx(), 
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
+      }
+    }),
+    ...whenExternalScripts(() => partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    }))
+  ],
   image: {
     domains: ["cdn.pixabay.com"]
   },
--- src/navigation.js
+++ src/navigation.js
@@ -14,10 +14,6 @@ export const headerData = {
       text: 'Conversion Rate Optimierung',
       href: getPermalink('/conversionrate'),
     },
-    {
-      text: 'Über mich',
-      href: getPermalink('/about'),
-    },
     /**
     {
       text: 'Landing',
